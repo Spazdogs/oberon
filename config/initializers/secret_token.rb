203247fecb67364188c4aa5476eae8b7bafb2145
spazdogs <samuel.mason@me.com>
2011-04-18 01:00:38 -0500

    initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oberon::Application.config.secret_token = 'f3b470f3c15248fc533ae5bffdff78cc6e4512cce432ec00ebb6789c81849dd0c638513ad37ada9fe0216c0f08b1d4fa844c054c269349a414c32518c88dc962'
